497956e0ba739f4964c71ec46802bc4828bb05ad
willeaton101 <weaton@princeton.edu>
2022-03-01 09:01:31 -0500

    Global blobs working

--- global.py
+++ global.py
@@ -1,79 +1,102 @@
-import matplotlib.pyplot as plt
-import numpy as np
 from gen_sphere import gen_sphere
 from model_class import model
 from add_sphere import addSphere
 import scipy
+import numpy as np
+from scipy.interpolate import griddata as gd
+import netCDF4 as nc
 
-def norm(x):
-    return x/np.amax(np.abs(x))
+def latlon_to_cartesian(lat, long, depth, e2, a):
+    # Convert the ellipsoidal (geographic) coordinates to x,y,z:
+    lat = np.radians(lat)
+    lon = np.radians(long)
 
-sph_coord_sph = (1300000, 35, 0)     # 3000 km depth, 20 degrees N, 50 degrees E
-sph_rad = 4000                        # 10 km
+    N = a / np.sqrt(1 - e2 * (np.sin(lat) ** 2))
+    X = (N - depth) * np.cos(lat) * np.cos(lon)
+    Y = (N - depth) * np.cos(lat) * np.sin(lon)
+    Z = ((1 - e2) * N - depth) * np.sin(lat)
+    return X, Y, Z, N
 
-# Convert the ellipsoidal (geographic) coordinates to x,y,z:
-d  = sph_coord_sph[0]
-lat = np.radians(sph_coord_sph[1])
-lon = np.radians(sph_coord_sph[2])
-e2  = 6.69437999014*(10**(-3))  # WGS84
-a = 6378137.0
-b = 6356752.3
-N = a/np.sqrt(1 - e2*(np.sin(lat)**2))
+def cartesian_to_ellipsoidal(x,y,z, e2, iterations=50, z_def="depth"):
 
-e2  =0  # WGS84
-a = 6371000.0
-b = 6371000.0
+    # Now need to convert each coordinate back to geographic:
+    p = ((x ** 2 + y ** 2) ** 0.5)
+    lat_i = np.arctan(z / ((1 - e2) * p))
 
 
-X = (N - d )*np.cos(lat)*np.cos(lon)
-Y = (N - d)*np.cos(lat)*np.sin(lon)
-Z = ((1-e2)*N - d)*np.sin(lat)
+    for i in range(iterations):
+        N = a / (1 - e2 * (np.sin(lat_i) ** 2)) ** 0.5
+        depth = p / np.cos(lat_i) - N
+        lat_i = np.arctan(z / (p * (1 - (e2 * (N / (N + depth))))))
+
+    # This is now our lat, long, depth coordinates for our blob
+    depth = -depth  # Flip sign so depth is positive
+    if z_def=="radius":
+        print("Using radius for coordinates not depth")
+        depth = N-depth
+    longitude = np.rad2deg(np.arctan(y / x))
+    latitude = np.rad2deg(lat_i)
+
+    return depth, latitude, longitude
+
+
+
+# SPHERE PARAMETERS:
+sph_depth          =  1000000  # m
+sph_lat            =  0     # degrees
+sph_long           =  0     # degrees
+sph_radius         =  200000   # mm
+padding            =  1.2
+oversat            =  12
+cart_gridspace     =  130
+
+property_type  = "REF1D"
+vp             = -0.2
+vs             = -0.2
+rho            = -0.2
+
+# DOMAIN PARAMETERS
+freq           =  0.01   # [Hz]
+min_vel        =  5000   # [m/s]
+epw            =  4      # 3 elements per wavelength
+
+# ELLIPTICITY STUFF
+e2             =  0
+a              =  6371000.0
+
+
+
+# Convert to cartesian
+X, Y, Z, N = latlon_to_cartesian(sph_lat, sph_long, sph_depth, e2, a)
 
 # Convert centre to xyz:
 # Model parameters
-freq = 2        # [Hz]
-min_vel = 8000  # [m/s]
-epw = 3         # 3 elements per wavelength
-
-p = sph_rad*2.0
+p = sph_radius*padding
 # Model dimensions
-x = np.array([-p, p]) + X # Model from -150,000 [m] to +150,000 [m] in x direction
-y = np.array([-p, p]) + Y # Model from -150,000 [m] to +150,000 [m] in y direction
-z = np.array([-p, p]) + Z # Model from -100,000 [m] to +100,000 [m] in z direction
+x = np.array([-p, p]) + X
+y = np.array([-p, p]) + Y
+z = np.array([-p, p]) + Z
 
-m = model(x,y,z, epw, freq, min_vel, oversaturation=5)
 
-sph = gen_sphere(model=m, radius=sph_rad, RHO=2500, VP=2500, VS=2300)
+# NOW UTILISE CARTESIAN SCRIPTS
+m = model(x, y, z, epw, freq, min_vel, oversaturation=oversat)
+sph = gen_sphere(model=m, radius=sph_radius, RHO=rho, VP=vp, VS=vs)
 sph.set_centre(np.array([X,Y,Z]), m, print_conf='n')
-# Add sphere to model:
 m = addSphere(sph, m)
 
-# Now need to convert our cartesian back to geographic
+
+print("Created cartesian model.")
 x_array = np.linspace(m.x_lim[0], m.x_lim[1], m.nx)
 y_array = np.linspace(m.y_lim[0], m.y_lim[1], m.ny)
 z_array = np.linspace(m.z_lim[0], m.z_lim[1], m.nz)
 
-X,Y,Z = np.meshgrid(x_array, y_array, z_array)
+XC, YC, ZC = np.meshgrid(x_array, y_array, z_array)
+x = XC.flatten()
+y = YC.flatten()
+z = ZC.flatten()
 
-x = X.flatten()
-y = Y.flatten()
-z = Z.flatten()
-
-# Now need to convert each coordinate back to geographic:
-p = ((x**2 + y**2)**0.5)
-
-lat_i = np.arctan(z / ((1-e2)*p))
-
-n=50
-for i in range(n):
-    N = a/(1 - e2*(np.sin(lat_i))**2)**0.5
-    h = p/np.cos(lat_i) - N
-    lat_i = np.arctan( z/( p*(1 - (e2*(N/(N+h)))) ) )
-
-# This is now our lat, long, depth coordinates for our blob
-longitude = np.rad2deg(np.arctan(y/x))
-latitude = np.rad2deg(lat_i)
-h = -h  # Flip sign so depth is positive
+h, latitude, longitude = cartesian_to_ellipsoidal(x, y, z, e2, z_def='radius')
+print("Converted back to ellipsoidal coords.")
 
 
 # Now need to re-sample the data at even spacing in lat-lon-depth space:
@@ -85,106 +108,114 @@ depth_min = np.min(h)
 depth_max = np.max(h)
 
 
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-from scipy.interpolate import griddata as gd
-import time
+vp  = m.bm_vp.flatten()
+vs  = m.bm_vs.flatten()
+rho = m.bm_rho.flatten()
 
 
-#grid = np.zeros(K.shape)
-
-v = m.bm_vp.flatten()
-x = latitude[v!=0]
-y = longitude[v!=0]
-z = h[v!=0]
-v = v[v!=0]
+x = latitude[vp!=0]
+y = longitude[vp!=0]
+z = h[vp!=0]
+v = vp[vp!=0]
 
 
+print("Resampling with even grid")
 # Prescribe grid in regular lat-long-depth:
-n = 80
-k = np.arange(lat_min, lat_max+(lat_max-lat_min)/n, (lat_max-lat_min)/n)
-l = np.arange(lon_min, lon_max+ (lon_max-lon_min)/n, (lon_max-lon_min)/n)
-j = np.arange(depth_min, depth_max+ (depth_max-depth_min)/n, (depth_max-depth_min)/n)
-K, L, J = np.meshgrid(k, l, j)
+grid_lat = np.arange(lat_min, lat_max+(lat_max-lat_min)/cart_gridspace, (lat_max-lat_min)/cart_gridspace)
+grid_lon = np.arange(lon_min, lon_max+ (lon_max-lon_min)/cart_gridspace, (lon_max-lon_min)/cart_gridspace)
+grid_depth = np.arange(depth_min, depth_max+ (depth_max-depth_min)/cart_gridspace, (depth_max-depth_min)/cart_gridspace)
 
-"""
-dist_min = np.infty
-# Now for each scattered data point we want to find its nearest grid point
-for p in range(len(v)):
-    px = x[p]
-    py = y[p]
-    pz = z[p]
+K, L, J = np.meshgrid(grid_lat, grid_lon, grid_depth)
 
-    for kp in k:
-        for lp in l:
-            for jp in j:
-                dist = ((px - kp)**2 + (py - lp)**2 + (pz - jp)**2)**0.5
-                if dist < dist_min:
-                    dist_min = np.copy(dist)
-                    min_x = np.copy(px)
-                    min_y = np.copy(py)
-                    min_z = np.copy(pz)
-"""
+# Interpolate sphere in new grid system:
+interp_vp = scipy.interpolate.griddata(points=(latitude, longitude, h), values=vp,
+                                 xi = (K.flatten(), L.flatten(), J.flatten()), method='nearest', rescale=True)
+print("     resampled vp")
+interp_vs = scipy.interpolate.griddata(points=(latitude, longitude, h), values=vs,
+                                 xi = (K.flatten(), L.flatten(), J.flatten()), method='nearest', rescale=True)
+print("     resampled vs")
+interp_rho = scipy.interpolate.griddata(points=(latitude, longitude, h), values=rho,
+                                 xi = (K.flatten(), L.flatten(), J.flatten()), method='nearest', rescale=True)
+print("     resampled rho")
 
-interp = scipy.interpolate.griddata(points=(latitude, longitude, h), values=m.bm_vp.flatten(),
-                           xi = (K.flatten(), L.flatten(), J.flatten()), method='nearest',
-                           rescale=True)
-
-fig1 = plt.figure()
-ax1=fig1.gca(projection='3d')
-
-ax1.scatter(x,y,z, c=v)
+# Reshape into 3D array for NETCDF
+vp  = interp_vp.reshape(len(grid_lat), len(grid_lon), len(grid_depth))   # May need to change this order
+vs  = interp_vs.reshape(len(grid_lat), len(grid_lon), len(grid_depth))   # May need to change this order
+rho = interp_rho.reshape(len(grid_lat), len(grid_lon), len(grid_depth)) # May need to change this order
 
 
-fig2 = plt.figure()
-ax2=fig2.gca(projection='3d')
+print("exporting to NetCDF4")
 
-ax2.scatter(K.flatten()[interp>0], L.flatten()[interp>0], J.flatten()[interp>0], c=interp[interp>0])
+out_dir  = f"../../AxiSEM3D_2020/build/input/"
+filename = f"sphere_{sph_depth}d_{sph_lat}lat_{sph_long}lon_{sph_radius}rad.nc"
+fullname = f"{out_dir}/{filename}"
 
-
-
-plt.show()
-
-
-
-
-
-
-
-
-
-
-import netCDF4 as nc
-"""
-fname = 'test1.nc'
-
-f = nc.Dataset(fname, 'w', format='NETCDF4')
+f = nc.Dataset(fullname, 'w', format='NETCDF4')
 # Create dimension arrays
 # We now create the dimensions:
-lat_dim = f.createDimension('lat_dim', len(latitude))
-lon_dim = f.createDimension('lon_dim', len(longitude))
-depth_dim = f.createDimension('depth_dim', len(h))
+lat = f.createDimension('lat', len(grid_lat))
+lon = f.createDimension('lon', len(grid_lon))
+depth = f.createDimension('depth', len(grid_depth))
 # Creating the variables:
-lat     = f.createVariable('lat', 'f4', ('lat_dim',))
-lat.units = 'degrees_north'
-lat.long_name = 'latitude'
-lon     = f.createVariable('lon', 'f4', ('lon_dim',))
-lon.units = 'degrees_east'
-lon.long_name = 'longitude'
-depth   = f.createVariable('depth', 'f4', ('depth_dim',))
-depth.units = 'metres'
-depth.long_name = 'depth'
-v_rho = f.createVariable('rho', 'f4', ('lat_dim',))    # Pretty sure they should all be the same dimension
-v_vp  = f.createVariable('vp', 'f4', ('lat_dim',))
-v_vs  = f.createVariable('vs', 'f4', ('lat_dim',))
+lats = f.createVariable('lat', 'f4', ('lat',))
+lats.units = 'degrees_north'
+lats.long_name = 'latitude'
+
+lons = f.createVariable('lon', 'f4', ('lon',))
+lons.units = 'degrees_east'
+lons.long_name = 'longitude'
+
+depths = f.createVariable('depth', 'f4', ('depth',))
+depths.units = 'meters'
+
+v_rho = f.createVariable('rho', 'f4', ('lat', 'lon', 'depth',))
+v_vp = f.createVariable('vp', 'f4', ('lat', 'lon', 'depth',))
+v_vs = f.createVariable('vs', 'f4', ('lat', 'lon', 'depth',))
+
 # Assigning values to the variables:
-lat[:]   = latitude
-lon[:]   = longitude
-depth[:] = h
-v_rho[:] = m.bm_rho
-v_vp[:]  = m.bm_vp.flatten()
-v_vs[:]  =  m.bm_vs.flatten()
-print('Data written to file ', fname)
-f.close()"""
+lats[:] = grid_lat
+lons[:] = grid_lon
+depths[:] = grid_depth
+v_rho[:, :, :] = rho
+v_vp[:, :, :] = vp
+v_vs[:, :, :] = vs
+print('Data written to file ', filename)
+f.close()
+
+
+
+# Write corresponding YAML content:
+print("COPY INTO inparam.model.yaml file:")
+print("")
+print(f"##############################################################################################################")
+print(f"    - {filename[:-3]}:")
+print("       activated: true")
+print("       class_name: StructuredGridV3D")
+print(f"       nc_data_file: {filename}")
+print(f"       coordinates:")
+print(f"           horizontal: LATITUDE_LONGITUDE")
+print(f"           vertical: DEPTH")
+print(f"           ellipticity: false")
+print(f"           depth_below_solid_surface: false")
+print(f"           nc_variables: [lat, lon, depth]")
+print(f"           data_rank: [1, 2, 0]")
+print(f"           length_unit: m")
+print(f"           angle_unit: degree")
+print(f"           undulated_geometry: false")
+print(f"           # note: setting whole_element_inplane to true often causes errors for these blobs")
+print(f"           whole_element_inplane: false")
+print(f"       properties:")
+print(f"           - VP:")
+print(f"               nc_var: vp")
+print(f"               factor: 1")
+print(f"               reference_kind: {property_type}")
+print(f"           - VS:")
+print(f"               nc_var: vs")
+print(f"               factor: 1")
+print(f"               reference_kind: {property_type}")
+print(f"           - RHO:")
+print(f"               nc_var: rho")
+print(f"               factor: 1")
+print(f"               reference_kind: {property_type}")
+print(f"       store_grid_only_on_leaders: true")
+print(f"##############################################################################################################")
